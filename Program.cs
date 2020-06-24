141b109291cfcd0530c7cdf8b7de8bc75397bd18
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-06-24 15:13:34 -0600

    Add an assignment; output concatenated hello string.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, TechCareers!");
+            //Two slashes mark the begining of a singlr-line comment.
+            //We are assigning a string value to a variable.
+            string myName = "Shailza Sharma";
+             /* We begin multi-line comments with forward-slash and asterisk...
+             Used concatenantion operator to add variable value to text "Hello".
+            ...and we end multi-line comments with an asterisk followed by a forward-slash */
+            Console.WriteLine("Hello, " + myName);
+           
         }
     }
 }
