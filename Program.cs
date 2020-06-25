20f5f55a6c76f43bddf765ca25907cd1231dee08
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-06-25 15:16:47 -0600

    added for and for each loop.

--- Program.cs
+++ Program.cs
@@ -6,23 +6,29 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Enter a number:");
-           int userInt = Int32.Parse(Console.ReadLine());
-           if(userInt > 10)
+           int myNum = 3;
+           while (myNum > 0 )  // Iterates/loops while condition is true.
            {
-            Console.WriteLine("Greater than 10.");
+               Console.WriteLine("Current number is : {0}", myNum);
+               myNum--;
            }
-           else if (userInt > 5)
-           {
-            Console.WriteLine("Greater than 5 but less than 11.");
-           }
-            else if ( userInt < 0 )
+            /* For loops have 3 semi colon separated components:
+            1) Assignment.
+            2) Termination Condition.
+            3) Iteration.
+            */
+            for(int i=0; i<6; i+=2)
             {
-                Console.WriteLine( "A negative number." );
+             Console.WriteLine("Current (for) number is : {0}", i);   
             }
-            else
+
+            string[] myList = {"cat","turtle","lizard","frog","dog"};
+
+            // @link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/foreach-in
+
+            foreach(string listItem in myList )
             {
-                Console.WriteLine( "Number is between 0 and 5." );
+                Console.WriteLine("The current item is: {0}", listItem);
             }
            }
            }
